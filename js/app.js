2179b29d57a2949c7ee55babefad6b012ffeb1cd
badas-cs <yatcotr@gmail.com>
2026-06-19 23:33:31 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function showPage(pageId){
 
 document.querySelectorAll(".page")
-.forEach(page => {
+.forEach(page=>{
 
 page.classList.remove("active");
 
@@ -17,7 +17,7 @@ function initializeDatabase(){
 
 if(!localStorage.getItem("users")){
 
-const users = [
+const users=[
 
 {
 id:1,
@@ -35,6 +35,124 @@ JSON.stringify(users)
 );
 
 }
+
+}
+
+function register(){
+
+const username =
+document.getElementById(
+"registerUsername"
+).value;
+
+const password =
+document.getElementById(
+"registerPassword"
+).value;
+
+if(!username || !password){
+
+alert("Tüm alanları doldur.");
+
+return;
+
+}
+
+let users = JSON.parse(
+localStorage.getItem("users")
+);
+
+const exists = users.find(
+u=>u.username===username
+);
+
+if(exists){
+
+alert("Bu kullanıcı zaten var.");
+
+return;
+
+}
+
+users.push({
+
+id:Date.now(),
+
+username,
+
+password,
+
+role:"user",
+
+balance:1000
+
+});
+
+localStorage.setItem(
+"users",
+JSON.stringify(users)
+);
+
+alert("Kayıt başarılı.");
+
+showPage("loginPage");
+
+}
+
+function login(){
+
+const username =
+document.getElementById(
+"loginUsername"
+).value;
+
+const password =
+document.getElementById(
+"loginPassword"
+).value;
+
+let users = JSON.parse(
+localStorage.getItem("users")
+);
+
+const user = users.find(
+
+u=>
+
+u.username===username &&
+u.password===password
+
+);
+
+if(!user){
+
+alert("Hatalı giriş.");
+
+return;
+
+}
+
+localStorage.setItem(
+"currentUser",
+JSON.stringify(user)
+);
+
+document.getElementById(
+"balance"
+).innerText =
+user.balance;
+
+showPage("dashboardPage");
+
+}
+
+function logout(){
+
+localStorage.removeItem(
+"currentUser"
+);
+
+showPage("loginPage");
 
 }
 
